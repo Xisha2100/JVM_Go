4a9e3acbd23578c7d23d20b44d6f06885dff46e9
Noah <32444724+Xisha2100@users.noreply.github.com>
2021-04-10 02:15:32 +0800

    new

--- src/top/nzhz/HeapOOM.java
+++ src/top/nzhz/HeapOOM.java
@@ -9,7 +9,7 @@ public class HeapOOM {
     }
 
     public static void main(String[] args) {
-        List<OOMObject> list = new ArrayList<OOMObject>();
+        List<OOMObject> list = new ArrayList<>();
 
         while (true){
             list.add(new OOMObject());
