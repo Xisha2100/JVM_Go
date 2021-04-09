3889ec444457acfc38da6fcf85f7c3d5a40acdfb
Noah <32444724+Xisha2100@users.noreply.github.com>
2021-04-10 02:14:19 +0800

    new

---
+++ src/top/nzhz/HeapOOM.java
@@ -0,0 +1,18 @@
+package top.nzhz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeapOOM {
+    static class OOMObject {
+        static int a=5;
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<OOMObject>();
+
+        while (true){
+            list.add(new OOMObject());
+        }
+    }
+}
