6597bb8724ab8e4d3e1909e7ffdb91c22908fb9f
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2021-12-28 00:36:54 +0800

    byte code

--- src/top/nzhz/JavaMethodAreaOOM.java
+++ src/top/nzhz/JavaMethodAreaOOM.java
@@ -2,30 +2,30 @@ package top.nzhz;
 
 //VM_Args:-XX:PermSize=10M -XX:MaxPermSize=10M
 
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
-
-import java.lang.reflect.Method;
-
-public class JavaMethodAreaOOM {
-
-    public static void main(String[] args) {
-        while (true) {
-            var enhancer = new Enhancer();
-            enhancer.setSuperclass(OOMObject.class);
-            enhancer.setUseCache(false);
-            enhancer.setCallback(new MethodInterceptor() {
-                @Override
-                public Object intercept(Object o, Method method, Object[] args, MethodProxy proxy) throws Throwable {
-                    return proxy.invokeSuper(o,args);
-                }
-            });
-            enhancer.create();
-        }
-    }
-
-    static class OOMObject {
-
-    }
-}
+//import net.sf.cglib.proxy.Enhancer;
+//import net.sf.cglib.proxy.MethodInterceptor;
+//import net.sf.cglib.proxy.MethodProxy;
+//
+//import java.lang.reflect.Method;
+//
+//public class JavaMethodAreaOOM {
+//
+//    public static void main(String[] args) {
+//        while (true) {
+//            var enhancer = new Enhancer();
+//            enhancer.setSuperclass(OOMObject.class);
+//            enhancer.setUseCache(false);
+//            enhancer.setCallback(new MethodInterceptor() {
+//                @Override
+//                public Object intercept(Object o, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+//                    return proxy.invokeSuper(o,args);
+//                }
+//            });
+//            enhancer.create();
+//        }
+//    }
+//
+//    static class OOMObject {
+//
+//    }
+//}
